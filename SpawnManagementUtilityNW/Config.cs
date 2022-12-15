8ce8dbb8b0d5bd21b261323f4726493534b9b7f0
undid-iridium <francis.x.irizarry@gmail.com>
2022-12-15 10:05:43 -0500

    I am blind

---
+++ SpawnManagementUtilityNW/Config.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace SpawnManagementUtilityNW
+{
+    public class Config
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether debug logs should be shown.
+        /// </summary>
+        [Description("Whether debug logs should be shown.")]
+        public bool Debug { get; set; } = false;
+    }
+}
---
+++ SpawnManagementUtilityNW/SpawnManagementUtility.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using PluginAPI.Core;
+using PluginAPI.Core.Attributes;
+
+namespace SpawnManagementUtilityNW
+{
+    public class SpawnManagementUtility
+    {
+        [PluginConfig]
+        public Config Config;
+        
+        /// <summary>
+        /// Gets a static instance of the <see cref="SpawnManagementUtility"/> class.
+        /// </summary>
+        public static SpawnManagementUtility Instance { get; private set; }
+        
+        
+        [PluginEntryPoint("SpawnManagementUtilityNW", "1.0.0", 
+            "CLean up items by zone, item, and time", "Undid Iridium")]
+        void LoadPlugin()
+        {
+            Instance = this;
+       
+            PluginAPI.Events.EventManager.RegisterEvents(this);
+            Log.Debug("We have started our plugin CleanupUtilityNW!!", Instance.Config.Debug);
+        }
+        
+    }
+}
