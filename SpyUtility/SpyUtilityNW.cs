77dd03ef9b959fb0959858d0bd29c5f38eca4e72
undid-iridium <francis.x.irizarry@gmail.com>
2022-12-21 23:32:52 -0500

    Correct name

--- SpyUtility/SpyUtilityNW.cs
+++ SpyUtility/SpyUtilityNW.cs
@@ -22,7 +22,7 @@ namespace SpyUtilityNW
         public SpyManager SpyManager { get; set; }
 
         [PluginEntryPoint("SpyUtilityNW", "1.0.0",
-            "CI Spy plugin", "Undid Iridium")]
+            "Spy plugin", "Undid Iridium")]
         private void LoadPlugin()
         {
             Instance = this;
@@ -31,30 +31,12 @@ namespace SpyUtilityNW
                 harmony = new Harmony($"com.Undid-Iridium.SpyUtilityNW.{DateTime.UtcNow.Ticks}");
                 harmony.PatchAll();
 
-                // SpyManager = new CISpyManager();
                 PluginAPI.Events.EventManager.RegisterEvents(this);
                 PluginAPI.Events.EventManager.RegisterEvents<SpyManager>(this);
                 PatchedEventHandlers.BeforeTeamRespawn += SpyManager.OnWaveSpawn;
                 
-
                 Log.Debug("We have started our plugin SpyUtilityNW!!", SpyUtilityNW.Instance.Config.Debug);
             }
-        }
-
-
-        //public static MethodBase TargetMethod() => typeof(Scp079Camera).GetMethods(BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.DeclaredOnly).First(method => method.Name.Contains("OnDestroy"));
-
-        [PluginEvent(ServerEventType.TeamRespawn)]
-        private void onWaveSpawn(SpawnableTeamType spawnTeamType)
-        {
-            Log.Debug(
-                $"What is ServerEventType.TeamRespawn {spawnTeamType == SpawnableTeamType.NineTailedFox} and blah", SpyUtilityNW.Instance.Config.Debug);
-        }
-
-        [PluginEvent(ServerEventType.TeamRespawnSelected)]
-        private void onWaveRespawn(SpawnableTeamType spawnTeamType)
-        {
-            Log.Debug("What is ServerEventType.TeamRespawnSelected and blah", SpyUtilityNW.Instance.Config.Debug);
         }
     }
 }
