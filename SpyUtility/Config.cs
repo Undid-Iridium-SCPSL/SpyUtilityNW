c338722623ed6f88c5a4ca5fc038c4e8b9b72edd
undid-iridium <francis.x.irizarry@gmail.com>
2022-12-23 16:55:14 -0500

    Added configuration to specify duration of hint, and messages.

--- SpyUtility/Config.cs
+++ SpyUtility/Config.cs
@@ -44,5 +44,75 @@ namespace SpyUtilityNW
         /// </summary>
         [Description("Gets or sets a value determining how many attempts to spawn a spy can occur.")]
         public int HowManyRetries { get; set; } = 3;
+
+
+        /// <summary>
+        ///     Gets or sets a string that is shown when a spy is created.
+        /// </summary>
+        [Description("Gets or sets a string that is show when a spy is created. You must provide a {0} in your string to place the team name")]
+        public string OnSpySpawnMessage { get; set; } =
+            "<align=center><voffset=28em> <color=#F6511D> Congrats, you're a spy for {0} team </color></voffset></align>";
+        
+        /// <summary>
+        /// How long to show <see cref="OnSpySpawnMessage"/> for.
+        /// </summary>
+        [Description("How long to show OnSpySpawnMessage for.")]
+        public float OnSpySpawnMessageHintDuration { get; set; } = 3f;
+        
+        /// <summary>
+        ///     Gets or sets a string that is shown when attacking a spy hiding on the enemy team
+        /// </summary>
+        [Description("Gets or sets a string that is shown when spy attacking real teammate. You must provide a {0} in your string to place the team name")]
+        public string OnSpyAttackingTeammate { get; set; } =
+            "<align=center><voffset=28em> <color=#F6511D> You're on {0} team, remember? </color></voffset></align>";
+        
+        /// <summary>
+        /// How long to show <see cref="OnSpyAttackingTeammate"/> for.
+        /// </summary>
+        [Description("How long to show OnSpySpawnMessage for.")]
+        public float OnSpyAttackingTeammateHintDuration { get; set; } = 3f;
+
+        /// <summary>
+        ///     Gets or sets a string that is shown when attacking a spy hiding on the enemy team
+        /// </summary>
+        [Description("Gets or sets a string that is shown when attacking a spy hiding on the enemy team. You must provide a {0} in your string to place the team name")]
+        public string OnTeammateAttackingSpy { get; set; } = "<align=center><voffset=28em> <color=#F6511D> They're on {0} team, remember? </color></voffset></align>";
+
+               
+        /// <summary>
+        /// How long to show <see cref="OnTeammateAttackingSpy"/> for.
+        /// </summary>
+        [Description("How long to show OnSpySpawnMessage for.")]
+        public float OnTeammateAttackingSpyHintDuration { get; set; } = 3f;
+
+        /// <summary>
+        ///     Gets or sets a string that is shown when a spy is revealed. 
+        /// </summary>
+        [Description("Gets or sets a string that is shown when a spy is revealed.")]
+        public string SpyHasBeenRevealed { get; set; } =
+            "<align=center><voffset=28em> <color=#F6511D> You've been revealed!!! </color></voffset></align>";
+        
+        /// <summary>
+        /// How long to show <see cref="SpyHasBeenRevealed"/> for.
+        /// </summary>
+        [Description("How long to show OnSpySpawnMessage for.")]
+        public float SpyHasBeenRevealedHintDuration { get; set; } = 3f;
+
+
+        /// <summary>
+        ///     Gets or sets a string that is shown when a spy's on the same real team attack each other.
+        /// </summary>
+        [Description("Gets or sets a string that is shown when a spy's on the same real team attack each other.")]
+        public string SameTeamSpyMessage { get; set; } =
+            "<align=center><voffset=28em> <color=#F6511D> That's a fellow spy! You can't attack them </color></voffset></align>";
+        
+        /// <summary>
+        /// How long to show <see cref="SpyHasBeenRevealed"/> for.
+        /// </summary>
+        [Description("How long to show OnSpySpawnMessage for.")]
+        public float SameTeamSpyMessageHintDuration { get; set; } = 3f;
+
+
+ 
     }
 }
